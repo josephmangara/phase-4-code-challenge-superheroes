f982a6cf28dff76efcc6536f324e5383e9073131
josephmangara <josephmangara2@gmail.com>
2024-01-29 10:35:24 +0300

    routes

--- server/app.py
+++ server/app.py
@@ -17,40 +17,53 @@ def home():
 
 @app.route('/heroes', methods=['GET'])
 def heroes():
-    heroes = [hero.to_dict() for hero in Hero.query.all()]
+    heroes = []
+    for hero in Hero.query.all():
+        hero_dict = {
+            'id': hero.id,
+            'name': hero.name,
+            'super_name': hero.super_name,
+            'created_at': hero.created_at,
+            'updated_at': hero.updated_at,
+            'powers': [{'id': hp.power.id, 'name': hp.power.name, 'description': hp.power.description, 'strength': hp.strength} for hp in hero.hero_powers]
+        }
+        heroes.append(hero_dict)
+
     return jsonify(heroes), 200
 
 @app.route('/heroes/<int:id>')
 def hero_by_id(id):
     hero = Hero.query.filter(Hero.id == id).first()
-    hero_dict = hero.to_dict()
-
-    response = make_response(
-        hero_dict,
-        200
-        )
-    
-    return response
-
-    # hero = Hero.query.get(id)
-    # if hero:
-    #     return jsonify(hero.to_dict()), 200
-    # else:
-    #     return jsonify({"error": "Hero not found"}), 404
+    if hero:
+        hero_dict = {
+            'id': hero.id,
+            'name': hero.name,
+            'super_name': hero.super_name,
+            'created_at': hero.created_at,
+            'updated_at': hero.updated_at,
+            'powers': [{'id': hp.power.id, 'name': hp.power.name, 'description': hp.power.description, 'strength': hp.strength} for hp in hero.hero_powers]
+        }
+        return jsonify(hero_dict), 200
+    else:
+        return jsonify({'error': 'Hero not found'}), 404
 
 @app.route('/powers')
 def powers():
     powers = []
     for power in Power.query.all():
-        powers_dict = power.to_dict()
-        powers.append(powers_dict)
+        power_dict = {
+            'id': power.id,
+            'name': power.name,
+            'description': power.description,
+            'created_at': power.created_at,
+            'updated_at': power.updated_at,
+            'heroes': [{'id': hp.hero.id, 'name': hp.hero.name, 'super_name': hp.hero.super_name, 'strength': hp.strength} for hp in power.power_heroes]
+        }
+        powers.append(power_dict)
 
-    response = make_response(
-        jsonify(powers),
-        200
-    )
+    return jsonify(powers), 200
 
-    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
